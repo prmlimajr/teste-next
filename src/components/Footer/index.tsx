802ea08b0432b602bf1e7c45b9862a818b8fcc95
Paulo Lima <prmlimajr@hotmail.com>
2021-09-16 18:08:52 -0300

    Create Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  Container,
+  Signature
+} from './styles';
+
+export function Footer() {
+  return (
+    <Container>
+      <Signature>
+        Desenvolvido por <a href="https://github.com/prmlimajr">Paulo Lima</a>
+      </Signature>
+    </Container>
+  );
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  width: 100%;
+  background: var(--color-card);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  height: 100px;
+`;
+
+export const Signature = styled.span`
+  font-size: 1.4rem;
+`;
